8f4faefb6344b418f39df7f97a2f962ebff2260e
ben.harvey1213 <ben.harvey1213@gmail.com>
2020-05-04 21:52:54 -0500

    adding listing fix

--- frontend/src/app/listing-card/listing-card.component.ts
+++ frontend/src/app/listing-card/listing-card.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ListingObject } from '../listing-object';
 import { DataInteractionService } from '../data-interaction.service';
 import { Router } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-listing-card',
@@ -12,16 +13,26 @@ export class ListingCardComponent implements OnInit {
 
   @Input() listingObject : ListingObject;
 
-  constructor(private dataInteractionService : DataInteractionService, private router : Router) { }
+  constructor(private db : AngularFirestore, private dataInteractionService : DataInteractionService, private router : Router) { }
 
   ngOnInit(): void {
   }
 
   remove(){
-    this.dataInteractionService.removeListing(this.listingObject);
+    this.db.collection('products').ref.get().then(res => {
+      res.forEach(product => {
+        if (product.data()['price'] == this.listingObject.price && product.data()['name'] == this.listingObject.description && product.data()['photoUrl'] == this.listingObject.imageUrl){
+          let id = product.id
 
-    // reload page
-    this.router.navigateByUrl('profile');
+          this.db.collection('products').doc(id).delete().then(res => {
+            this.router.navigateByUrl('');
+          });
+
+        }
+      })
+    });
+
+    
   }
 
 }
